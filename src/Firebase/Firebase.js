ccaa085660feb9e9bf2e4f0f54f93820b51be204
abircu <mdabircse4873@gmail.com>
2023-10-04 21:05:07 +0600

    created a news portal side using react js

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4SSNsXAS8sfymIv-z_gTJb-rKcMxsMH8",
+  authDomain: "ajker-news-bd.firebaseapp.com",
+  projectId: "ajker-news-bd",
+  storageBucket: "ajker-news-bd.appspot.com",
+  messagingSenderId: "230429198651",
+  appId: "1:230429198651:web:6f6549dc34c8087d6d4f1e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
