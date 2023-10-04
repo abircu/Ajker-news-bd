866b61422981ce53d505c619d4cbc4287d310466
abircu <mdabircse4873@gmail.com>
2023-10-04 22:26:45 +0600

    set time date & year dainamically

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Header from "../../Shared/Header/Header";
 import Navbar from "../../Shared/Navbar/Navbar";
+import RightSideNav from "../../Shared/RightSideNav/RightSideNav";
+import LeftSideNav from "../../Shared/LeftSideNav/LeftSideNav";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Navbar></Navbar>
-      <h2 className="text-2xl font-bold font-poppins">this is home page</h2>
+      <div className="grid grid-col-1 md:grid-cols-4 gap-6">
+        <div className="grid ">
+          <LeftSideNav></LeftSideNav>
+        </div>
+        <div className=" col-span-2">
+          <h2 className="text-3xl">Main content</h2>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,7 +1,17 @@
+import logo from "../../assets/logo.png";
+import moment from "moment";
 const Header = () => {
   return (
     <div>
-      <h1> htis header</h1>
+      <div className="flex justify-center items-center mt-12 mb-5">
+        <img src={logo} alt="" />
+      </div>
+      <p className="text-center text-xl text-[#706F6F]">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-xl  font-medium text-[#403F3F] text-center mb-2">
+        {moment().format("dddd, MMMM D, YYYY")}
+      </p>
     </div>
   );
 };
