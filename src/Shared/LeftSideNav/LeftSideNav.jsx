8e09d812239e88316a06016ef55c5a5f2fe48ca8
abircu <mdabircse4873@gmail.com>
2023-10-06 19:34:24 +0600

    left nev complete

--- src/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+  const [categories, setCategory] = useState([]);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
   return (
     <div>
-      <h1 className="text-2xl">left side nav</h1>
+      <h1 className="text-2xl">All Categories:{categories.length}</h1>
+      {categories.map((category) => (
+        <Link
+          className="block  p-4 xl font-bold text-[#9F9F9F]"
+          key={category.id}
+          to={`${category.id}`}
+        >
+          {category.name}
+        </Link>
+      ))}
     </div>
   );
 };
