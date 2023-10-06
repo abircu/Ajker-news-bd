48047415c68a53eacbd5acc3497beaa263f4b270
abircu <mdabircse4873@gmail.com>
2023-10-06 20:38:41 +0600

    login page crate and route setup completed

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../Shared/Navbar/Navbar";
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2>Login page </h2>
+    </div>
+  );
+};
+
+export default Login;
